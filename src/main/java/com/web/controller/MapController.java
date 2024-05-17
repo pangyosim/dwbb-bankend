1d9ceaa66a8572019d64dba35a309ee3619d8e66
simpangyo <spg9468@naver.com>
2024-05-17 13:00:59 +0900

    Update API ERROR Exception

--- src/main/java/com/web/controller/MapController.java
+++ src/main/java/com/web/controller/MapController.java
@@ -74,46 +74,67 @@ public class MapController {
             rd.close();
             conn.disconnect();
             JSONParser parser = new JSONParser();
-            Object obz = parser.parse(sb.toString());
-            JSONObject obj = (JSONObject)obz;
-            JSONArray wait_arr = (JSONArray) obj.get("brcdList");
-            // DB에 저장되어있는 은행정보 불러오기
-            List<Bank> bank_data = bs.findAll();
-            JSONArray tmp_arr = new JSONArray();
-            // 현재 위치에서 5km이내 은행 조회
-            // for-loop
-            for(Bank obb : bank_data){
-                double distance = Math.round(Haversine_formula_method(bk.getGeox(), bk.getGeoy(), obb.getGeoy(), obb.getGeox()) * 100) / 100.0;
-                if( distance < 5.0 ) {
-                    obb.setDistance(distance);
-                    tmp_arr.add(obb);
-                }
-            }
-            JSONArray res = new JSONArray();
-            if(!wait_arr.isEmpty()) {
-                for (Object wait_obj : wait_arr) {
-                    JSONObject wait_json_obj = (JSONObject) wait_obj;
-                    for (Object tmp_obj : tmp_arr) {
-                        Bank tmp_bank_obj = (Bank) tmp_obj;
-                        if (tmp_bank_obj.getBrcd().equals(wait_json_obj.get("brcd"))) {
-                            JSONObject tmp_json_obj = new JSONObject();
-                            tmp_json_obj.put("brcd", tmp_bank_obj.getBrcd());
-                            tmp_json_obj.put("krnbrm", tmp_bank_obj.getKrnbrm());
-                            tmp_json_obj.put("brncnwbscadr", tmp_bank_obj.getBrncnwbscadr());
-                            tmp_json_obj.put("brncTel", tmp_bank_obj.getBrncTel());
-                            tmp_json_obj.put("rprsFax", tmp_bank_obj.getRprsFax());
-                            tmp_json_obj.put("geox", tmp_bank_obj.getGeox());
-                            tmp_json_obj.put("geoy", tmp_bank_obj.getGeoy());
-                            tmp_json_obj.put("distance", tmp_bank_obj.getDistance());
-                            tmp_json_obj.put("tlwnList", wait_json_obj.get("tlwnList"));
-                            res.add(tmp_json_obj);
-                        }
+            // API 응답 데이터에 따라 catch 예외 던지기
+            try{
+                Object obz = parser.parse(sb.toString());
+                JSONObject obj = (JSONObject)obz;
+                JSONArray wait_arr = (JSONArray) obj.get("brcdList");
+                // DB에 저장되어있는 은행정보 불러오기
+                List<Bank> bank_data = bs.findAll();
+                JSONArray tmp_arr = new JSONArray();
+                // 현재 위치에서 5km이내 은행 조회
+                // for-loop
+                for(Bank obb : bank_data){
+                    double distance = Math.round(Haversine_formula_method(bk.getGeox(), bk.getGeoy(), obb.getGeoy(), obb.getGeox()) * 100) / 100.0;
+                    if( distance < 5.0 ) {
+                        obb.setDistance(distance);
+                        tmp_arr.add(obb);
                     }
                 }
-            } else {
-                res.addAll(tmp_arr);
+                JSONArray res = new JSONArray();
+                if(!wait_arr.isEmpty()) {
+                    for (Object wait_obj : wait_arr) {
+                        JSONObject wait_json_obj = (JSONObject) wait_obj;
+                        for (Object tmp_obj : tmp_arr) {
+                            Bank tmp_bank_obj = (Bank) tmp_obj;
+                            if (tmp_bank_obj.getBrcd().equals(wait_json_obj.get("brcd"))) {
+                                JSONObject tmp_json_obj = new JSONObject();
+                                tmp_json_obj.put("brcd", tmp_bank_obj.getBrcd());
+                                tmp_json_obj.put("krnbrm", tmp_bank_obj.getKrnbrm());
+                                tmp_json_obj.put("brncnwbscadr", tmp_bank_obj.getBrncnwbscadr());
+                                tmp_json_obj.put("brncTel", tmp_bank_obj.getBrncTel());
+                                tmp_json_obj.put("rprsFax", tmp_bank_obj.getRprsFax());
+                                tmp_json_obj.put("geox", tmp_bank_obj.getGeox());
+                                tmp_json_obj.put("geoy", tmp_bank_obj.getGeoy());
+                                tmp_json_obj.put("distance", tmp_bank_obj.getDistance());
+                                tmp_json_obj.put("tlwnList", wait_json_obj.get("tlwnList"));
+                                res.add(tmp_json_obj);
+                            }
+                        }
+                    }
+                } else {
+                    res.addAll(tmp_arr);
+                }
+                System.out.println(res);
+                return res;
+            } catch (ParseException pe){
+                JSONArray API_err = new JSONArray();
+                List<Bank> bank_data = bs.findAll();
+                JSONArray tmp_arr = new JSONArray();
+                // 현재 위치에서 5km이내 은행 조회
+                // for-loop
+                for(Bank obb : bank_data){
+                    double distance = Math.round(Haversine_formula_method(bk.getGeox(), bk.getGeoy(), obb.getGeoy(), obb.getGeox()) * 100) / 100.0;
+                    if( distance < 5.0 ) {
+                        obb.setDistance(distance);
+                        tmp_arr.add(obb);
+                    }
+                }
+                API_err.add("Bank API 점검");
+                API_err.addAll(tmp_arr);
+                return API_err;
+
             }
-            return res;
             // stream
 //            bank_data.stream()
 //                    .filter(obb -> {
